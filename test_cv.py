8529c88b7d4794d940362021295cc7e7bfe8ee06
Omar Faruk <omarfaruk20@iut-dhaka.edu>
2023-09-20 11:34:36 +0600

    up

--- test_cv.py
+++ test_cv.py
@@ -9,6 +9,39 @@ qt_error()
 def video(source):
     return cv2.VideoCapture(source)
 
-# cap = video(0)
+def video_break(wait = 1 , exit_key = 'q'):
+    return cv2.waitKey(wait) == ord(exit_key)
 
-# def show_video(cap , title = 'Frame' , wait = 0):
+
+def show_video(cap , title = 'Frame' , wait = 1 , exit_key = 'q'):
+    while True:
+        ret, frame = cap.read()
+        
+        cv2.imshow("Video", frame)
+        # show_cv(cap , title="Video Capture" , raw = True)
+        if video_break():
+            break
+
+def fill_cv(shape , value):
+    # return np.zeros(shape , np.uint8)
+    return np.ones(shape , np.uint8)*value
+
+cap = video(0)
+
+# show_video(cap)
+
+while True:
+    ret, face = cap.read()
+
+    frame = fill_cv(face.shape , 0)
+    face = resize_cv( face , scale=0.5 )
+
+    frame[0:face.shape[0] , 0:face.shape[1]] = face
+    frame[face.shape[0]: , face.shape[1]:] = rotate_cv(face , -2)
+    frame[face.shape[0]: , 0:face.shape[1]] = resize_cv( rotate_cv(face , -1) , face.shape[0] , face.shape[1] )
+    
+    cv2.imshow("Video" , frame)
+
+    if video_break():
+        break
+
